5423b98a8efb34ef9a666132a4d542a77b3ac0f2
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-04-21 22:25:43 +0100

    fix linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -3,11 +3,13 @@ module MyEnumerable
     each { |item| return false unless block.call(item) }
     true
   end
+
   def filter(&block)
     arr = []
     each { |item| arr << item if block.call(item) }
     arr
   end
+
   def any?(&block)
     each { |item| return true if block.call(item) }
     false
